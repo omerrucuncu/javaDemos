1ea20e52546f07a1754cef84eb293e835fa7deb0
omerucuncu <118854548+omerrucuncu@users.noreply.github.com>
2022-11-25 02:27:20 +0300

    java alıştırmalar
    javakamp 2.gün ödevler

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class CustomerManager {
+
+	public void Add() {
+		System.out.println("Eklendi");
+	}
+
+	public void Remove() {
+		System.out.println("Silindi");
+	}
+
+	public void Update() {
+		System.out.println("Güncellendi");
+	}
+}
